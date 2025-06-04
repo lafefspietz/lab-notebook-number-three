38bc5944f318a286e2272fa3dda49f4c87ecab83
Lafe Spietz <lafe.f.spietz@nist.gov>
2025-06-04 11:00:35 -0600

    water bottle

---
+++ water-bottle/local-replicator.php
@@ -0,0 +1,39 @@
+<?php
+
+$dnaraw = file_get_contents("../dna.txt");
+$dna = json_decode($dnaraw);
+$baseurl = "../";
+
+mkdir("php");
+
+copy("../php/local-replicator.txt","replicator.php");
+
+foreach($dna->html as $value){
+    
+    copy($baseurl.$value,$value);
+
+}
+
+foreach($dna->php as $value){
+ 
+    copy($baseurl."php/".$value,"php/".$value);
+    copy($baseurl."php/".$value,explode(".",$value)[0].".php");
+
+}
+    
+
+
+?>
+<a href = "index.html">CLICK ME(3/3)</a>
+<style>
+body{
+    background-color:BLACK;
+    font-family:Comic Sans MS;
+    font-size:3em;
+}
+a{
+        font-size:3em;
+        color:#ff2cb4;
+
+}
+</style>
