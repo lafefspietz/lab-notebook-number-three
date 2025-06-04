fac3198baae29acdb8048d69d9ad2a1fb3d54a67
Lafe Spietz <lafe.f.spietz@nist.gov>
2025-06-04 08:15:43 -0600

    Constructing basic structure of object

---
+++ README.md
@@ -0,0 +1,19 @@
+# [web of things]()
+  
+  ![](qrcode.png)
+  
+   - [index.html](index.html)
+   - [http://localhost/lab-notebook-number-three/](http://localhost/lab-notebook-number-three/)
+   - [branch.html](branch.html)
+   - [edit-html.html](edit-html.html)
+   - [edit-index.html](edit-index.html)
+   - [edit-php.html](edit-php.html)
+   - [freebox.html](freebox.html)
+   - [qrcode.html](qrcode.html)
+   - [qrcode-page.html](qrcode-page.html)
+   - [generate-dna.php](generate-dna.php)
+   - [delete-php.html](delete-php.html)
+   - [DOWNLOAD GITHUB DESKTOP](https://desktop.github.com/download/)
+   - [GET A GITHUB ACCOUNT](https://github.com/)
+   - [INSTALL XAMPP](https://www.apachefriends.org/)
+   
---
+++ generate-dna.php
@@ -0,0 +1,50 @@
+<!-- 
+this program generates the file dna.txt which lists the files to replicate 
+-->
+<a style ="font-family:Comic Sans MS;color:blue;font-size:1.5em;" href = "index.html">index.html</a>
+
+<br>
+<a style ="font-family:Comic Sans MS;color:blue;font-size:1.5em;" href = "edit-html.html">edit-html.html</a>
+<br>
+<a style ="font-family:Comic Sans MS;color:blue;font-size:1.5em;" href = "edit-php.html">edit-php.html</a>
+<br>
+<a style ="font-family:Comic Sans MS;color:blue;font-size:1.5em;" href = "edit-book.html">edit-book.html</a>
+<br>
+
+
+<br/>
+<pre>
+<?php
+
+    $files = scandir(getcwd());
+    $phpfiles = scandir(getcwd()."/php");
+
+
+    $htmlfiles = [];
+    foreach($files as $value){
+        if(substr($value,-4) == ".txt" || substr($value,-4) == ".css" || substr($value,-5) == ".html" || substr($value,-3) == ".md" || substr($value,-3) == ".py" || substr($value,-3) == ".sh" || substr($value,-3) == ".js"){
+            array_push($htmlfiles,$value);
+        }
+    }
+
+    $dna = json_decode("{}");
+    $dna->html = $htmlfiles;
+
+
+    $dna->php = [];
+    foreach($phpfiles as $value){
+        if($value[0] != "."){
+            array_push($dna->php,$value);
+        }
+    }
+
+
+
+    echo json_encode($dna,JSON_PRETTY_PRINT);
+
+    $file = fopen("dna.txt","w");// create new file with this name
+    fwrite($file,json_encode($dna,JSON_PRETTY_PRINT)); //write data to file
+    fclose($file);  //close file
+
+?>
+</pre>
